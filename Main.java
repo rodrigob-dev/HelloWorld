7a3863bda43c5defd29e07734a0d2a49119d0796
Rodrigo Barcelos <rodrigolunabarcelos@gmail.com>
2025-05-16 11:29:49 -0300

    Finds total, odd and even total from array of numbers

--- Main.java
+++ Main.java
@@ -3,11 +3,19 @@ public class Main {
     public static void main(String[] args) {
         int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int total = 0;
+        int pares = 0;
+        int impares = 0;
         for (int i = 0; i < array.length; i++) {
             total += array[i];
+            if (array[i] % 2 == 0) {
+                pares ++;
+            } else {
+                impares++;
+            }
         }
-        System.out.println(total);
-    
-        
+        System.out.println("Total: " + total);
+        System.out.println("Total even numbers: " + pares);
+        System.out.println("Total odd numbers: " + impares);
+      
     }
 }
