235a906be7976f25f8dd91d634ce647ca833b1d4
Rodrigo Barcelos <rodrigolunabarcelos@gmail.com>
2025-05-16 11:03:30 -0300

    Name Reader

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Escreva seu nome: ");
+        String name = input.nextLine();
+        System.out.println("Olá " + name + "!");
+        input.close();
     }
 }
